2d2edcf1b749dd91968ff8e4720e1bd0f592990f
Manu S D <6363manumanu@gmail.com>
2026-08-28 12:16:09 +0530

    Secure database configuration

--- flight_booking/settings.py
+++ flight_booking/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -78,11 +82,11 @@ WSGI_APPLICATION = 'flight_booking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'flight_booking_db',
-        'USER': 'postgres',
-        'PASSWORD': 'MyPassword123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
